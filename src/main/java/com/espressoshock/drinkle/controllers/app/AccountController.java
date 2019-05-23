01cd14a71a4059d5f48c36dd2fd9b20718aac6f3
Sergheev Andrian <sergheev@icloud.com>
2019-05-23 21:28:42 +0200

    initial account view

--- src/main/java/com/espressoshock/drinkle/controllers/app/AccountController.java
+++ src/main/java/com/espressoshock/drinkle/controllers/app/AccountController.java
@@ -1,4 +1,40 @@
 package com.espressoshock.drinkle.controllers.app;
 
-public class AccountController {
+import com.espressoshock.drinkle.appState.Current;
+import com.espressoshock.drinkle.models.AccountType;
+import com.espressoshock.drinkle.viewLoader.EventDispatcherAdapter;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javax.swing.text.html.ImageView;
+
+
+public class AccountController extends EventDispatcherAdapter {
+
+  @FXML
+  TextField emailTextField, passwordTextField, nameTextField;
+
+  @FXML
+  ImageView logoImageView;
+
+  @FXML
+  public void initialize() {
+    setupComponents();
+    System.out.println("Account view loaded");
+  }
+  
+
+  private void setupComponents() {
+    AccountType type = Current.environment.userType();
+
+    if (type == AccountType.Private) {
+      // set image for private
+    } else if (type == AccountType.Business){
+      // set image for business
+    }
+  }
+
+
+
+
+
 }
